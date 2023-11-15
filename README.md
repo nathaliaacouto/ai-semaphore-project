d37ad62610f3bd3ea68a85c7a7572324e601853d
lorenaaseabra <lorenamseabra@gmail.com>
2023-11-15 15:39:26 +0000

    New functionalities and readme
    Added CentralCoordinationAgent, DisruptionManagementAgent, EmergencyVehicleAgent

--- README.md
+++ README.md
@@ -1,1 +1,120 @@
-# ai-semaphore-project
+
+# Traffic Management System 
+
+## Introduction
+
+This document provides an overview and explanation of the Traffic Management System, a simulation of traffic flow at intersections. The system incorporates various agents, including traffic lights, vehicles, and central coordination to optimize traffic flow, predict disruptions, and prioritize emergency vehicles.
+
+## Components
+
+### 1. TrafficLight Class
+
+Represents a traffic light at an intersection.
+
+- Attributes:
+  - `color`: Current color of the traffic light (Green, Yellow, Red).
+  - `time`: Duration of each traffic light state.
+
+- Methods:
+  - `change_color`: Changes the traffic light color based on the current state.
+  - `change_time`: Updates the duration of each traffic light state.
+
+### 2. Vehicle Class
+
+Simulates a vehicle approaching an intersection.
+
+- Attributes:
+  - `waiting_time`: Time the vehicle has been waiting at the intersection.
+  - `position`: Current position of the vehicle.
+
+- Methods:
+  - `stop`: Stops the vehicle if the traffic light is red.
+  - `request_green_light`: Requests a green light if waiting time exceeds a threshold.
+
+### 3. RoadSign Class
+
+Represents a road sign indicating traffic rules.
+
+- Attributes:
+  - `sign_type`: Type of road sign (e.g., Go, Yield, Stop).
+
+### 4. Lane Class
+
+Represents a lane on a road.
+
+- Attributes:
+  - `lane_id`: Identifier for the lane.
+  - `vehicles`: List of vehicles in the lane.
+
+- Methods:
+  - `add_vehicle`: Adds a vehicle to the lane.
+
+### 5. Road Class
+
+Represents a road with multiple lanes.
+
+- Attributes:
+  - `name`: Name of the road.
+  - `lanes`: List of lanes on the road.
+
+### 6. Intersection Class
+
+Represents an intersection with traffic lights and road signs.
+
+- Attributes:
+  - `name`: Name of the intersection.
+  - `traffic_light`: TrafficLight instance controlling the intersection's traffic light.
+  - `road_sign`: RoadSign instance indicating traffic rules.
+  - `road`: Road instance associated with the intersection.
+
+- Methods:
+  - `change_road_sign`: Updates the road sign based on the traffic light color.
+  - `check_change_with_time`: Checks if it's time to change the traffic light color.
+
+### 7. CentralCoordinationAgent Class
+
+Coordinates traffic flow by adjusting traffic light timings.
+
+- Attributes:
+  - `intersections`: List of intersections to coordinate.
+
+- Methods:
+  - `optimize_traffic_flow`: Adjusts traffic light timings based on traffic conditions.
+
+### 8. DisruptionManagementAgent Class
+
+Predicts disruptions and communicates with other agents.
+
+- Methods:
+  - `predict_disruptions`: Predicts disruptions and communicates with Traffic Light Agents.
+
+### 9. EmergencyVehicleAgent Class
+
+Manages emergency vehicle priority.
+
+- Methods:
+  - `request_priority`: Requests priority for emergency vehicles and communicates with Traffic Light Agents.
+
+### 10. Environment Class
+
+Represents the overall environment with multiple roads and intersections.
+
+- Attributes:
+  - `roads`: List of roads in the environment.
+  - `intersections`: List of intersections in the environment.
+  - `central_agent`: CentralCoordinationAgent instance.
+  - `disruption_agent`: DisruptionManagementAgent instance.
+  - `emergency_agent`: EmergencyVehicleAgent instance.
+
+- Methods:
+  - `add_road`: Adds a road to the environment.
+  - `add_intersection`: Adds an intersection to the environment.
+  - `display`: Displays the current state of roads and intersections.
+
+## Execution
+
+The system is executed by creating an instance of the Environment class and calling the `display` method. During execution, the environment simulates traffic flow, adjusts traffic light timings, predicts disruptions, and handles emergency vehicle priorities.
+
+## Conclusion
+
+The Traffic Management System provides a modular and extensible framework for simulating and optimizing traffic flow at intersections. Each component plays a specific role, contributing to a comprehensive simulation of a real-world traffic management scenario. The system's flexibility allows for further enhancements and customizations to meet specific requirements and scenarios.
--- traffic_agents.py
+++ traffic_agents.py
@@ -59,10 +59,15 @@ class Road:
 
 
 class Intersection:
+    #Adds a new attribute road to the Intersection class, representing a road associated with each intersection.The road is initialized as an instance of the Road class with two lanes.
+
     def __init__(self, name, traffic_light_color, traffic_light_time, road_sign_type):
         self.name = name
         self.traffic_light = TrafficLight(traffic_light_color, traffic_light_time)
         self.road_sign = RoadSign(road_sign_type)
+
+        # Additional attributes
+        self.road = Road(f"{name}_Road", 2)  # Each intersection has a two-lane road
 
     def change_road_sign(self):
         if self.traffic_light.color == GREEN_LIGHT:
@@ -79,6 +84,82 @@ class Intersection:
             self.traffic_light.change_color()
 
 
+class CentralCoordinationAgent:
+   #Introduces the CentralCoordinationAgent class responsible for coordinating and optimizing traffic flow.
+    #The optimize_traffic_flow method performs the following actions:
+        #Adjusts traffic light timings based on traffic conditions.
+        #Communicates with other agents, such as Vehicle Agents, to improve traffic flow.
+
+    def __init__(self, intersections):
+        self.intersections = intersections
+
+    def optimize_traffic_flow(self):
+        # Additional logic to optimize traffic flow
+        print("Optimizing traffic flow")
+
+        # Example: Adjust traffic light timings based on traffic conditions
+        for intersection in self.intersections:
+            if intersection.traffic_light.color == GREEN_LIGHT:
+                intersection.traffic_light.change_time(intersection.traffic_light.time - 1)
+            elif intersection.traffic_light.color == RED_LIGHT:
+                intersection.traffic_light.change_time(intersection.traffic_light.time + 1)
+
+            # Additional logic to communicate with other agents (e.g., Vehicle Agents)
+            for lane in intersection.road.lanes:
+                for vehicle in lane.vehicles:
+                    if vehicle.waiting_time > 20:
+                        print(f"Communicating with Vehicle at Lane {lane.lane_id}: "
+                              f"Adjusting waiting time for better flow.")
+
+
+class DisruptionManagementAgent:
+    #Introduces the DisruptionManagementAgent class to handle disruption prediction and management.
+    #The predict_disruptions method performs the following actions:
+        #Placeholder for disruption prediction logic using a machine learning model.
+        #If the disruption probability is greater than 0.5, alerts relevant agents and adjusts traffic light timings.
+
+
+    def __init__(self):
+        # Additional initialization for disruption management
+        print("Disruption Management Agent initialized")
+
+    def predict_disruptions(self):
+        # Additional logic to predict disruptions
+        print("Predicting disruptions")
+
+        # Example: Placeholder logic for disruption prediction using a machine learning model
+        disruption_probability = 0.2  # Placeholder probability
+        if disruption_probability > 0.5:
+            print("Disruption predicted! Alerting relevant agents.")
+
+            # Additional logic to communicate with other agents (e.g., Traffic Light Agents)
+            print("Communicating with Traffic Light Agents: Adjusting timings for predicted disruption.")
+
+
+class EmergencyVehicleAgent:
+
+    #Introduces the EmergencyVehicleAgent class to handle emergency vehicles.
+    #The request_priority method performs the following actions:
+        #Placeholder for logic to request priority based on emergency conditions.
+        #If there is an emergency condition, requests priority and communicates with traffic lights to obtain it.
+    def __init__(self):
+        # Additional initialization for emergency vehicle agent
+        print("Emergency Vehicle Agent initialized")
+
+    def request_priority(self):
+        # Additional logic for emergency vehicle priority
+        print("Requesting priority for emergency vehicle")
+
+        # Example: Placeholder logic to request priority based on emergency conditions
+        emergency_condition = True  # Placeholder for emergency condition
+        if emergency_condition:
+            print("Emergency vehicle requesting priority at intersections.")
+
+            # Additional logic to communicate with Traffic Light Agents
+            print("Communicating with Traffic Light Agents: Granting priority for emergency vehicle.")
+
+
+# Extend the Environment class to include the new agents
 class Environment:
     def __init__(self):
         road_1 = Road("Road_1", 2)  # 2 lanes
@@ -91,6 +172,15 @@ class Environment:
         self.roads = [road_1, road_2]
         self.intersections = [intersection_1, intersection_2]
 
+        # Additional agents
+        self.central_agent = CentralCoordinationAgent(self.intersections)
+        self.disruption_agent = DisruptionManagementAgent()
+        self.emergency_agent = EmergencyVehicleAgent()
+
+        # Populate lanes with vehicles for testing
+        road_1.lanes[0].add_vehicle(10, "Position_A")
+        road_2.lanes[1].add_vehicle(25, "Position_B")
+
     def add_road(self, road):
         self.roads.append(road)
 
@@ -98,22 +188,36 @@ class Environment:
         self.intersections.append(intersection)
 
     def display(self):
+        #Extends the display method to include additional functionalities:
+            #Calls the optimize_traffic_flow method of the CentralCoordinationAgent.
+            #Calls the predict_disruptions method of the DisruptionManagementAgent.
+            #Calls the request_priority method of the EmergencyVehicleAgent.
         start_time = t.time()
         for road in self.roads:
             print(f"{road.name}: ", end='')
             for lane in road.lanes:
                 print(f" Lane {lane.lane_id}", end='')
+                if lane.vehicles:
+                    print(" [Vehicles: ", end='')
+                    for vehicle in lane.vehicles:
+                        print(f"(Waiting Time: {vehicle.waiting_time}, Position: {vehicle.position}) ", end='')
+                    print("]", end='')
             print()
 
         print("Intersections: ")
-        for intersection in self.intersections: 
+        for intersection in self.intersections:
             intersection.check_change_with_time(start_time)
             print(
-                f"{intersection.name} - Traffic Light: {intersection.traffic_light.color}, Time: {intersection.traffic_light.time} "
+                f"{intersection.name} - Traffic Light: {intersection.traffic_light.color}, "
+                f"Time: {intersection.traffic_light.time} "
                 f"- Road Sign: {intersection.road_sign.sign_type}")
+
+        # Additional functionalities
+        self.central_agent.optimize_traffic_flow()
+        self.disruption_agent.predict_disruptions()
+        self.emergency_agent.request_priority()
 
 
 if __name__ == "__main__":
     env = Environment()
     env.display()
-    
